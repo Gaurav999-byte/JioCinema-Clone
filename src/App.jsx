007f1625476eac7a15b264aedff549e784558b29
Gaurav999-byte <nagralegaurav16@gmail.com>
2025-03-31 15:27:55 +0530

    Fixed routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Redirect from "/" to "/home" */}
+        {/* Redirect root "/" to "/home" */}
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
       </Routes>
