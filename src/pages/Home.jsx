8d4b3820a5c8481509b85b18511139bf7155b023
Gaurav999-byte <nagralegaurav16@gmail.com>
2025-03-31 15:02:02 +0530

    Reverted to previous working state of JioCinema Clone

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,71 +1,66 @@
-import Header from "../components/header/Header"
-import Tags from "../components/Tags/Tags"
-import Channels from "../components/channels/Channels"
-import Carousel from "../components/carousel/Carousel"
-import Featured from "../components/featured/Featured"
-import Shows from "../components/shows/Shows"
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import Header from "../components/header/Header";
+import Tags from "../components/Tags/Tags";
+import Channels from "../components/channels/Channels";
+import Carousel from "../components/carousel/Carousel";
+import Featured from "../components/featured/Featured";
+import Shows from "../components/shows/Shows";
 
+export default function Home() {
+  let [movies, setMovies] = useState([]);
+  let [featuredMovies, setFeaturedMovies] = useState([]);
+  let [dramaMovies, setDramaMovies] = useState([]);
+  let [hindiMovies, setHindiMovies] = useState([]);
+  let [topMovies, setTopMovies] = useState([]);
+  let [realityShows, setRealityShows] = useState([]);
 
-export default function Home()
-{
-    let [movies,setMovies] = useState([]);
-    let [featuredMovies,setFeaturedMovies] = useState([]);
-    let [dramaMovies,setDramaMovies] = useState([]);
-    let [hindiMovies,setHindiMovies] = useState([]);
-    let [topMovies,setTopMovies] = useState([]);
-    let [realityShows,setRealityShows] = useState([]);
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        let movieResponse = await fetch("http://localhost:3000/movies");
+        let moviesData = await movieResponse.json();
+        setMovies(moviesData);
 
-    useEffect(async()=>{
+        // Filter for featured movies
+        let featMovies = moviesData.filter((movie) => movie.featured === true);
+        setFeaturedMovies(featMovies.slice(0, 4));
 
-        try{
+        // Filter for Drama movies
+        let draMovies = moviesData.filter((movie) =>
+          movie.genre.includes("Drama")
+        );
+        setDramaMovies(draMovies.slice(0, 6));
 
-       let movieResponse =await fetch("http://localhost:3000/movies")
-       let moviesData = await movieResponse.json()
+        // Filter for Hindi movies
+        let hinMovies = moviesData.filter((movie) => movie.language === "Hindi");
+        setHindiMovies(hinMovies.slice(0, 6));
 
-       setMovies(moviesData)
+        // Filter for Top movies
+        let topRatedMovies = moviesData.filter((movie) => movie.imdb >= 8.5);
+        setTopMovies(topRatedMovies.slice(0, 6));
 
-       //filter for featured movies
-       let featMovies = moviesData.filter((movie)=>{return movie.featured==true})
-       console.log(featMovies)
-       setFeaturedMovies(featMovies.slice(0,4))
+        // Filter for Reality Shows
+        let realshows = moviesData.filter((movie) => movie.realindia === "reality");
+        setRealityShows(realshows.slice(0, 6));
+      } catch (err) {
+        console.log("Error fetching movies:", err);
+      }
+    };
 
-       //filter for Drama movies
-       let draMovies = moviesData.filter((movie)=>{return movie.genre.includes("Drama")})
-       console.log(draMovies)
-       setDramaMovies(draMovies.slice(0,6))
-      
-       //filter for Hindi movies
-       let hinMovies = moviesData.filter((movie)=>{return movie.language=="Hindi"})
-       console.log(hinMovies)
-       setHindiMovies(hinMovies.slice(0,6))
+    fetchMovies();
+  }, []);
 
-       //filter for Top movies
-       let topRatedMovies = moviesData.filter((movie)=>{return movie.imdb>=8.5})
-       setTopMovies(topRatedMovies.slice(0,6))
-
-       let realshows = moviesData.filter((movie)=>{return movie.realindia==="reality"})
-       setRealityShows(realshows.slice(0,6))
-
-        }
-        catch(err)
-        {
-            console.Console.log(err)
-        }
-
-    },[])
-
-    return(
-        <>
-            <Header movies ={movies}/>
-            <Tags/>
-            <Carousel/>
-            <Channels/>
-            <Featured movies={featuredMovies}/>
-            <Shows title ="Drama Movies" movies={dramaMovies}/>
-            <Shows title ="Hindi Movies" movies={hindiMovies}/>
-            <Shows title ="Top Movies" movies={topMovies}/>
-            <Shows title ="Reality Shows" movies={realityShows}/>
-        </>
-    )
+  return (
+    <>
+      <Header movies={movies} />
+      <Tags />
+      <Carousel />
+      <Channels />
+      <Featured movies={featuredMovies} />
+      <Shows title="Drama Movies" movies={dramaMovies} />
+      <Shows title="Hindi Movies" movies={hindiMovies} />
+      <Shows title="Top Movies" movies={topMovies} />
+      <Shows title="Reality Shows" movies={realityShows} />
+    </>
+  );
 }
