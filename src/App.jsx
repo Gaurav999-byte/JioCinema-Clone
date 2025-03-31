2981c6ffad6e776ea793d1c7cfb1644d05e9368f
Gaurav999-byte <nagralegaurav16@gmail.com>
2025-03-31 15:12:23 +0530

    Updated routing and fixed initial loading issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Routes,Route} from 'react-router-dom'
-
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-
   return (
-    <>
-     <BrowserRouter>
-     <Routes>
-            <Route path='/home' element={<Home/>}></Route>
-     </Routes>
-     </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Redirect from "/" to "/home" */}
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
